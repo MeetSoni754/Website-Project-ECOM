032934bcb57cb54f888b1fd7a98ff97fc84a2fcc
MetSoni754 <metsoni754@gmail.com>
2025-02-07 15:53:28 +0530

    eleven

--- JS/WEB-JS/Web.js
+++ JS/WEB-JS/Web.js
@@ -15,52 +15,59 @@ function checkScroll() {
 window.addEventListener('scroll', checkScroll);
 window.addEventListener('load', checkScroll);
 
-function clickme(){
-let element=document.querySelector(".divhide");
-let close=document.querySelector(".Hamburger");
-let open=document.querySelector(".close-1");
+function clickme() {
+    let element = document.querySelector(".divhide");
+    let close = document.querySelector(".Hamburger");
+    let open = document.querySelector(".close-1");
 
-close.style.display="none"
-open.style.display="block"
-element.style.display="block"
+    close.style.display = "none"
+    open.style.display = "block"
+    element.style.display = "flex"
+    let element2 = document.querySelector(".divhide");
+    if (element2.style.display == "flex");
+    element2.style.position = "fixed";
+    element2.style.top = "70px";
+    element2.style.left = "30%";
 
-//console.log(clc)
+    //console.log(clc)
 
 }
 
-function clickmeone(){
-    let element=document.querySelector(".divhide");
-let close=document.querySelector(".Hamburger");
-let open=document.querySelector(".close-1");
-    close.style.display="flex"
-open.style.display="none"
-element.style.display="none"
+function clickmeone() {
+    let element = document.querySelector(".divhide");
+    let close = document.querySelector(".Hamburger");
+    let open = document.querySelector(".close-1");
+    close.style.display = "flex"
+    open.style.display = "none"
+    element.style.display = "none"
+    element.style.position = "static";
 
-console.log(window.innerWidth)
+    console.log(window.innerWidth)
 }
-function sizechange(){
-    let element=document.querySelector(".divhide");
-let close=document.querySelector(".Hamburger");
-let open=document.querySelector(".close-1");
-    if(window.innerWidth>="768"){
-        close.style.display="none"
-        open.style.display="none"
-        element.style.display="none"
-    
+function sizechange() {
+    let element = document.querySelector(".divhide");
+    let close = document.querySelector(".Hamburger");
+    let open = document.querySelector(".close-1");
+    if (window.innerWidth >= "768") {
+        close.style.display = "none"
+        open.style.display = "none"
+        element.style.display = "none"
+
     }
-    else{
-        close.style.display="flex"
-        open.style.display="none"
-        element.style.display="none"
+    else {
+        close.style.display = "flex"
+        open.style.display = "none"
+        element.style.display = "none"
     }
-    
+
 
 }
 
-let ele=document.getElementById("Close-2");
+let ele = document.getElementById("Close-2");
 console.log(ele)
-ele.addEventListener("click",()=>{
+ele.addEventListener("click", () => {
 
-window.history.back();
+    window.history.back();
 
 })
+
